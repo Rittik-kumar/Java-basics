d350dddeb705e322a0ea2c61395d7666335377c2
Rittik Kumar <rkrittik17@gmail.com>
2020-12-23 20:31:30 +0530

    Print even odd numbers from array

---
+++ evenOddFromArray.java
@@ -0,0 +1,33 @@
+public class evenOddFromArray {
+  public static void main(String[] args) {
+    int a[]={1,2,3,4,5,6};
+
+    System.out.println("Even numbers in array.......");
+    for(int i=0;i<a.length;i++)
+    {
+       if(a[i]%2==0)
+       System.out.println(a[i]+" ");
+    }
+
+    System.out.println("Odd numbers in array.......");
+    for(int i=0;i<a.length;i++)
+    {
+       if(a[i]%2!=0)
+       System.out.println(a[i]+" ");
+    }
+
+    System.out.println("Even numbers in array.......");
+    for(int value:a)
+    {
+      if(value%2==0)
+      System.out.println(value+" ");
+    }
+
+    System.out.println("Odd numbers in array.......");
+    for(int value:a)
+    {
+      if(value%2!=0)
+      System.out.println(value+" ");
+    }
+  }
+}
